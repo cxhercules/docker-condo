559c7c74cc1aa4e7caa4951d12c975a6a304980a
Sunil Padmanabhan <psunil@corelogic.com>
2016-06-15 16:07:58 -0700

    Add files via upload

---
+++ scripts/rbenv-setup.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+# Install rbenv
+git clone https://github.com/rbenv/rbenv /usr/local/rbenv && \
+echo '# rbenv setup' > /etc/profile.d/rbenv.sh  && \
+echo 'export RBENV_ROOT=/usr/local/rbenv' >> /etc/profile.d/rbenv.sh  && \
+echo 'export PATH="$RBENV_ROOT/bin:$PATH"' >> /etc/profile.d/rbenv.sh  && \
+echo 'eval "$(rbenv init -)"' >> /etc/profile.d/rbenv.sh  && \
+chmod +x /etc/profile.d/rbenv.sh && \
+source /etc/profile.d/rbenv.sh && \
+pushd /tmp  && \
+git clone https://github.com/rbenv/ruby-build.git  && \
+cd  ruby-build  && \
+./install.sh  && \
+popd && \
+rbenv install $1 && \
+rbenv global $1 && \
+gem install bundle && \
+rbenv rehash 
